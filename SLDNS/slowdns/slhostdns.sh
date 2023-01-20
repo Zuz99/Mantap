2e1fdeb63093fe22f66755465c3052b22ae02bbf
Autis <andriautis@gmail.com>
2023-01-21 02:52:27 +0700

    Update slhostdns.sh

--- SLDNS/slowdns/slhostdns.sh
+++ SLDNS/slowdns/slhostdns.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/Zuz99
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
@@ -9,10 +9,10 @@ rm nsdomain
 
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-x0-9 | head -c5)
-DOMAIN=mantapxsl.my.id
-SUB_DOMAIN=onichan-${sub}.mantapxsl.my.id
-NS_DOMAIN=slowdns-${subsl}.mantapxsl.my.id
-CF_ID=slinfinity69@gmail.com
+DOMAIN=digvpn.my.id
+SUB_DOMAIN=onichan-${sub}.digvpn.my.id
+NS_DOMAIN=slowdns-${subsl}.digvpn.my.id
+CF_ID=andri.myid@gmail.com
 CF_KEY=dd2c5e0313f122b3c1833471d469b1025f492
 echo "IP=""$SUB_DOMAIN" >> /var/lib/crot/subdomain.conf
 set -euo pipefail
