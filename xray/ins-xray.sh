743ce120044db456996dd055e766b9eeee4233ff
Autis <79344663+Zuz99@users.noreply.github.com>
2023-02-07 00:58:03 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -18,6 +18,7 @@ domain=$(cat /etc/xray/domain)
 apt install iptables iptables-persistent -y
 apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
 apt install socat cron bash-completion ntpdate -y
+
 ntpdate pool.ntp.org
 apt -y install chrony
 timedatectl set-ntp true
